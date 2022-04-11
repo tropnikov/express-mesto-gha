e7845a45a3c1fc713c9b4f27fe6856f79b3064e2
Maksim Tropnikov <makstropnikov@gmail.com>
2022-04-12 01:50:33 +0700

    fixed avatar re

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const code = err.statusCode || 500;
-  res.status(code).send({ message: code === 500 ? 'Произошла внутренняя ошибка сервера' : err.message });
+  res.status(code).send({ message: code === 500 ? 'Произошла внутренняя ошибка сервера' : err.message, err });
 
   next();
 };
--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,15 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(pass) {
+        // eslint-disable-next-line
+        const re = /https?:\/\/(w{3}\.)?[-\w@:%.+~#=]+\.[\w()]+([-\w()@:%+.~#?&=\/]*)/;
+        return pass.match(re);
+      },
+    },
   },
   email: {
     type: String,
@@ -36,31 +44,28 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 8,
     select: false,
-    validate: {
-      validator(pass) {
-        // eslint-disable-next-line
-        const re = /https?:\/\/(w{3}\.)?[-\w@:%.+~#=]+\.[\w()]+([-\w()@:%+.~#?&=\/]*)/;
-        return pass.match(re);
-      },
-    },
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
+  return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+        return Promise.reject(
+          new UnauthorizedError('Неправильные почта или пароль'),
+        );
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UnauthorizedError('Неправильные почта или пароль'),
+          );
+        }
 
-          return user;
-        });
+        return user;
+      });
     });
 };
 
