b5f9bc52025e97545b9031b81b9a94f7de43709e
Maksim Tropnikov <makstropnikov@gmail.com>
2022-04-13 02:47:22 +0700

    added winston for logs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const auth = require('./middlewares/auth');
 const { register, signin } = require('./middlewares/validation');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,6 +17,8 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
+
+app.use(requestLogger); // логгер запросов
 
 app.post('/signin', signin, login);
 app.post('/signup', register, createUser);
@@ -28,6 +31,8 @@ app.use('/cards', cardsRoutes);
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена =('));
 });
+
+app.use(errorLogger); // логгер ошибок
 
 app.use(errors());
 
